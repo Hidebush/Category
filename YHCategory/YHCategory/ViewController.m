980a4996b8d65aa262972599c17ad844aa95ef1e
yuehuig <yuehuig@jumei.com>
2016-11-14 16:20:23 +0800

    检查文件、文件夹是否存在，不存在则创建

---
+++ YHCategory/YHCategory/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YHCategory
+//
+//  Created by 郭月辉 on 2016/11/14.
+//  Copyright © 2016年 Theshy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
