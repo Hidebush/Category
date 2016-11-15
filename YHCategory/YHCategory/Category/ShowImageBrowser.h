3aac2eb884f9471f17ad741704197464022add96
yuehuig <yuehuig@jumei.com>
2016-11-15 17:17:56 +0800

    点击 放大缩小图像

---
+++ YHCategory/YHCategory/Category/ShowImageBrowser.h
@@ -0,0 +1,17 @@
+//
+//  ShowImageBrowser.h
+//  YHCategory
+//
+//  Created by 郭月辉 on 2016/11/15.
+//  Copyright © 2016年 Theshy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowImageBrowser : NSObject
+
+/**
+ *  点击图像放大查看
+ */
++ (void)showImage:(UIImageView *)avatarImageView;
+@end
